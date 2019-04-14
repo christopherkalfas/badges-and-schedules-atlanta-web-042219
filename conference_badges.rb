95d9af94790e61d7e6ca2e481339279ca0634ae0
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-14 18:25:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,4 +20,9 @@ def assign_rooms(attendees)
   end 
   return room_assignment_array
 end 
-    
+   
+   
+ def printer(attendees) 
+   puts batch_badge_creator(attendees)
+   puts assign_rooms(attendees)
+  end 
