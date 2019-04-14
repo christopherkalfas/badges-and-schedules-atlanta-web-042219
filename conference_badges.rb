f264c7a06e904431303850f1e57ca8ab52c0c004
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-14 18:20:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def badge_maker(attendees)
+    return "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+  badge_array = []
+  attendees.each do |name|
+   badge_array << "Hello, my name is #{name}."
+  end 
+  return badge_array
+end 
+
+def assign_rooms(attendees)
+  room_number = 0
+  room_assignment_array = []
+  attendees.each do |name|
+    room_number += 1 
+    room_assignment_array << "Hello, #{name}! You'll be assigned to room #{room_number}!"
+  end 
+  return room_assignment_array
+end 
+    
